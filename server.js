6ebe7c16e92e95a028a679ca6a7e61f77aa822e4
Yihua Zhang <michaelzhang2@msn.com>
2019-07-07 16:39:21 -0400

    adding line to enable service worker

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(cors());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
+  app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+  });
+
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
